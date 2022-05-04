8b6c6243b5958e0aa52f8a42646f4ae8f34afa9e
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-04 14:54:24 +0200

    Added code to make it possible to send data between the two pages

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -1,14 +1,64 @@
 package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText username;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("==>", "MainActivity Created.");
+        username = findViewById(R.id.login_text);
+        Button signin = findViewById(R.id.sign_in_button);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "MainActivity Sign in button Pressed.");
+                Log.d("==>", "Username"+username.getText().toString());
+                Intent intent = new Intent(MainActivity.this, screen2.class);
+
+
+                intent.putExtra("username",username.getText().toString());
+                startActivity(intent);
+                intent.putExtra("username",username.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+    }
+    public MainActivity() {
+        super();
+    }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("==>", "MainActivity Resumed.");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>", "MainActivity Started.");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>", "MainActivity Stopped.");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>", "MainActivity Destroyed.");
     }
 }
+
