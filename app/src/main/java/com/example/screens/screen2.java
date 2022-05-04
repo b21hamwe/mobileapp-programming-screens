784e4e7b0dff705a3479203e1460f57cfd6cef67
Hampus Wetterlind <b21hamwe@student.his.se>
2022-05-04 14:54:46 +0200

    Added code to recieve data from the first page

--- app/src/main/java/com/example/screens/screen2.java
+++ app/src/main/java/com/example/screens/screen2.java
@@ -1,14 +1,65 @@
 package com.example.screens;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class screen2 extends AppCompatActivity {
+
+
+    private Button signout;
+    private TextView usernameView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen2);
+
+        signout = findViewById(R.id.sign_out_button);
+        usernameView = findViewById(R.id.username_text);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String username = extras.getString("username");
+        Log.d("==>", "Username from intent" + username);
+
+        usernameView.setText(username);
+
+        signout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "Sign out button");
+                finish();
+            }
+        });
+    }
+    public screen2() {
+        super();
+    }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("==>", "ProfileActivity Resumed.");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>", "ProfileActivity Started.");
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>", "ProfileActivity Stopped.");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>", "ProfileActivity Destroyed.");
     }
 }
